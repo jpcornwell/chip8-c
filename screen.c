3b30d6395c87ac42a9f9ada8513b1adc7382c89a
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-07-02 22:34:41 -0500

    Figure out how to display the pixel buffer using SDL

--- screen.c
+++ screen.c
@@ -7,11 +7,14 @@
 #define PIXEL_COUNT_WIDTH 64
 #define PIXEL_COUNT_HEIGHT 32
 
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 480;
+#define PIXEL_WIDTH 20
+#define PIXEL_HEIGHT 20
+#define WINDOW_WIDTH PIXEL_COUNT_WIDTH * PIXEL_WIDTH
+#define WINDOW_HEIGHT PIXEL_COUNT_HEIGHT * PIXEL_HEIGHT
+
 
 SDL_Window *window = NULL;
-SDL_Surface *screen_surface = NULL;
+SDL_Renderer *renderer = NULL;
 
 bool pixels[PIXEL_COUNT_WIDTH][PIXEL_COUNT_HEIGHT];
 
@@ -25,25 +28,47 @@ void init_display(void) {
         exit(1);
     }
 
-    screen_surface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format,
-                                                  0xff, 0xff, 0xff));
-    SDL_UpdateWindowSurface(window);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
 
-    SDL_Delay(2000);
+void update_display(void) {
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Rect pixel;
+
+    for (int x = 0; x < PIXEL_COUNT_WIDTH; x++) {
+        for (int y = 0; y < PIXEL_COUNT_HEIGHT; y++) {
+            if (pixels[x][y] == true) {
+                pixel.x = x * PIXEL_WIDTH;
+                pixel.y = y * PIXEL_HEIGHT;
+                pixel.w = PIXEL_WIDTH;
+                pixel.h = PIXEL_HEIGHT;
+
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderFillRect(renderer, &pixel);
+            }
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(1000);
 }
 
 void clear_display(void) {
     memset(pixels, 0, sizeof(pixels));
-    printf("Clear display stub\n");
+    update_display();
 }
 
 uint8_t draw_sprite(uint8_t x, uint8_t y, int n, uint8_t *addr) {
     uint8_t is_collision = 0;
+    uint8_t orig_x = x;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < 8; j++) {
-            if (((addr[i] >> j) & 1) == 0) {
+            if (((addr[i] >> (7 - j)) & 1) == 0) {
                 x = (x + 1) % PIXEL_COUNT_WIDTH;
                 continue;
             }
@@ -53,11 +78,11 @@ uint8_t draw_sprite(uint8_t x, uint8_t y, int n, uint8_t *addr) {
             pixels[x][y] ^= true;
             x = (x + 1) % PIXEL_COUNT_WIDTH;
         }
-        x = (x - 8) % PIXEL_COUNT_WIDTH;
+        x = orig_x;
         y = (y + 1) % PIXEL_COUNT_HEIGHT;
     }
 
-    printf("Draw to screen stub\n");
+    update_display();
 
     return is_collision;
 }
